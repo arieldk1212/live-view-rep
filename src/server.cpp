1bd50c8cd95573b6ca2a25a5cd3ac09d6efbfff3
Ariel Kriheli <arieldk6@gmail.com>
2024-10-09 13:11:49 +0300

    done vcpkg configurations, working.

--- src/server.cpp
+++ src/server.cpp
@@ -1,14 +1,8 @@
 #include "../inc/server.h"
-#include "../lib/Crow/include/crow.h"
+#include "crow.h"
 
 
 int main () {
   crow::SimpleApp app;
-
-  CROW_ROUTE(app, "/")
-  ([]() {
-    return "hello";
-  });
-
-  app.run(8000).run();
+  return 0;
 }
