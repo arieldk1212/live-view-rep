cc6a98737fb7cd62e97320a00c3e092e2b884ef5
Ariel Kriheli <arieldk6@gmail.com>
2024-11-14 16:40:41 +0200

    more work to be done..

--- inc/Core/Entity.h
+++ inc/Core/Entity.h
@@ -3,12 +3,12 @@
 
 #include "Type.h"
 
-// TODO: add logic for the data separation from the full address to entity block.
-// INFO: when i get the address from the Address Class, it will be passed down to Entity and
-// the logic of the address separation to entities will be dealt here, meaning i probably
-// want to change the Entity Ctor to the address itself, and the separate to Entities ( Data
-// Structures ).
-// INFO: the address class will inherit from entity due to the need use of the private entity
+// TODO: add logic for the data separation from the full address to entity
+// block. INFO: when i get the address from the Address Class, it will be passed
+// down to Entity and the logic of the address separation to entities will be
+// dealt here, meaning i probably want to change the Entity Ctor to the address
+// itself, and the separate to Entities ( Data Structures ). INFO: the address
+// class will inherit from entity due to the need use of the private entity
 // members and their functionality.
 
 class AbstractEntity {
@@ -21,7 +21,8 @@ public:
 
 template <typename EntityParam> class Entity : public AbstractEntity {
 public:
-  Entity(EntityParam &&ShortEntity, EntityParam &&LongEntity, Type &&EntityType);
+  Entity(EntityParam &&ShortEntity, EntityParam &&LongEntity,
+         Type &&EntityType);
   // Entity(Address Address);
   ~Entity() override;
 
