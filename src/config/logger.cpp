8fcdbef753fa6d934e41a4fec3a3539b22b14181
Ariel Kriheli <arieldk6@gmail.com>
2025-01-31 21:55:48 +0200

    more work

--- src/config/logger.cpp
+++ src/config/logger.cpp
@@ -7,28 +7,24 @@ void Logger::Init(const std::string &path) {
   std::string p_Path;
   p_Path = (path == "") ? p_Path = "../../backend-logs/" : p_Path = path;
 
-  std::vector<spdlog::sink_ptr> Sinks;
+  spdlog::sink_ptr app_sink =
+      std::make_shared<spdlog::sinks::basic_file_sink_mt>(p_Path + "app.log",
+                                                          true);
+  spdlog::sink_ptr system_sink =
+      std::make_shared<spdlog::sinks::basic_file_sink_mt>(p_Path + "sys.log",
+                                                          true);
 
-  Sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-      p_Path + "app.log", true));
-  Sinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-      p_Path + "sys.log", true));
+  app_sink->set_pattern("[%T] [%l] %n: %v");
+  system_sink->set_pattern("[%T] [%l] %n: %v");
 
-  Sinks[0]->set_pattern("[%T] [%l] %n: %v");
-  Sinks[1]->set_pattern("[%T] [%l] %n: %v");
+  s_AppLogger = std::make_shared<spdlog::logger>("APP", app_sink);
+  s_SystemLogger = std::make_shared<spdlog::logger>("SYSTEM", system_sink);
 
-  s_AppLogger =
-      std::make_shared<spdlog::logger>("APP", begin(Sinks), end(Sinks));
   spdlog::register_logger(s_AppLogger);
   s_AppLogger->set_level(spdlog::level::trace);
   s_AppLogger->flush_on(spdlog::level::trace);
 
-  s_SystemLogger =
-      std::make_shared<spdlog::logger>("SYSTEM", begin(Sinks), end(Sinks));
   spdlog::register_logger(s_SystemLogger);
   s_SystemLogger->set_level(spdlog::level::trace);
   s_SystemLogger->flush_on(spdlog::level::trace);
 }
-
-
-
